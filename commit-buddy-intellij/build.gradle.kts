11a4455f1afa4aeaa6d55ca801be2a0f18d3f0b8
sonjungchan <rnrmfjc@gmail.com>
2025-04-28 10:29:47 +0900

    [S12P31A101-52] feat: init project

---
+++ commit-buddy-intellij/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.9.25"
+    id("org.jetbrains.intellij") version "1.17.4"
+}
+
+group = "com.convi"
+version = "1.0-" +
+        "SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2024.1.7")
+
+    plugins.set(listOf("com.intellij.java", "Git4Idea"))
+}
+
+dependencies {
+    implementation ("dev.langchain4j:langchain4j:1.0.0-beta3")
+    implementation ("dev.langchain4j:langchain4j-ollama:1.0.0-beta3")
+    implementation ("org.testcontainers:ollama:1.20.4")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
+
+    testImplementation("org.testcontainers:junit-jupiter:1.19.6")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testImplementation("org.assertj:assertj-core:3.27.0")
+
+    implementation("org.tinylog:tinylog-impl:2.6.2")
+    implementation("org.tinylog:slf4j-tinylog:2.6.2")
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("241")
+        untilBuild.set("243.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
