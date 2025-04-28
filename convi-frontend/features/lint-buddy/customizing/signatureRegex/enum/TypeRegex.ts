7558838ec09b818be9fdd9dc5a04f795b9e9d9e6
jungchanSon <rnrmfjc@gmail.com>
2025-04-28 16:18:23 +0900

    [S12P31A101-60] feat: add regex component of scope, description, emoji

--- convi-frontend/features/lint-buddy/customizing/signatureRegex/enum/TypeRegex.ts
+++ convi-frontend/features/lint-buddy/customizing/signatureRegex/enum/TypeRegex.ts
@@ -1,7 +1,31 @@
-enum TypeRegex {
-    UPPER_CASE= `(?P<type>FEAT|FIX|DOCS|STYLE|REFACTOR|TEST|CHORE|PERF|BUILD|CI|REVERT|WIP|MERGE|RELEASE)`,
-    LOWER_CASE= `(?P<type>feat|fix|docs|style|refactor|test|chore|perf|build|ci|revert|wip|merge|release)`,
-    CAPITAL_CASE= `(?P<type>Feat|Fix|Docs|Style|Refactor|Test|Chore|Perf|Build|Ci|Revert|Wip|Merge|Release)`
+export enum Type {
+    UPPER_CASE= `(?<type>FEAT|FIX|DOCS|STYLE|REFACTOR|TEST|CHORE|PERF|BUILD|CI|REVERT|WIP|MERGE|RELEASE)`,
+    LOWER_CASE= `(?<type>feat|fix|docs|style|refactor|test|chore|perf|build|ci|revert|wip|merge|release)`,
+    CAPITAL_CASE= `(?<type>Feat|Fix|Docs|Style|Refactor|Test|Chore|Perf|Build|Ci|Revert|Wip|Merge|Release)`
 }
 
-export default TypeRegex
+export const Scope = `(?<type>.+)`
+
+export const Description = `(?<subject>.+)`
+export const Emoji = `(?<emoji>:[^:]+:\s)`
+export enum AngleBracket {
+    OPEN = `<`,
+    CLOSE = `>`,
+}
+
+export enum Brace {
+    OPEN = `\\{`,
+    CLOSE = `\\}`,
+}
+
+export enum Bracket  {
+    OPEN = `\\[`,
+    CLOSE = `\\]`,
+}
+
+export enum Parenthesis {
+    OPEN = `\\(`,
+    CLOSE = `\\)`,
+}
+
+export const Colon = `:`
