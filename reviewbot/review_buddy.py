5f8aed9c83d27f4427359d52db5ac6f5e01f08cc
sonjungchan <rnrmfjc@gmail.com>
2025-05-03 15:13:11 +0900

    [S12P31A101-68] feat: add dockerfile

--- reviewbot/review_buddy.py
+++ reviewbot/review_buddy.py
@@ -1,6 +1,28 @@
 import os
-
 import requests
+import ollama
+
+def requestOllama(prompt):
+    return ollama.generate(model="llama3.2", prompt=prompt)
+
+def requestLLM(prompt):
+    return requestOllama(prompt=prompt)
+
+def createPrompt(diff):
+    return f"""
+당신은 코드 리뷰어입니다.
+
+---  
+[변경된 코드]
+{diff}
+---  
+
+리뷰를 포인트별로 간결하게 작성해 주세요.
+"""
+
+def review(diff):
+    prompt = createPrompt(diff)
+    return requestLLM(prompt=prompt)
 
 def getDiffFromMR(host, projectId, state, privateToken, contentType, iid):
     requestURL = f"{host}/{projectId}/merge_requests/{iid}/changes?{state}"
@@ -8,7 +30,7 @@ def getDiffFromMR(host, projectId, state, privateToken, contentType, iid):
     response = requests.get(requestURL, headers = header)
     changes = response.json()["changes"]
 
-    print(changes)
+    print(review(changes))
 
     return changes
 
@@ -20,16 +42,14 @@ def getOpenedMR(host, projectId, state, privateToken, contentType):
 
 def main():
     HOST = "https://lab.ssafy.com/api/v4/projects"
-    PROJECT_ID = "1001960"
+    PROJECT_ID = os.getenv("CI_PROJECT_ID")
     STATE = "state=opened"
-    PRIVATE_TOKEN = "DT3XFH7Jx9JkKRymqv6X"
+    PRIVATE_TOKEN = os.environ.get("GITLAB_TOKEN")
     CONTENT_TYPE = "application/json"
-    pid = os.getenv("CI_PROJECT_ID")
-    print(os.getenv("CI_MERGE_REQUEST_IID"))
-    print(os.getenv("CI_MERGE_REQUEST_IID"))
-    print(pid)
-    print(pid)
-    getDiffFromMR(HOST, PROJECT_ID, STATE, PRIVATE_TOKEN, CONTENT_TYPE, 27)
+    IID = os.getenv("CI_MERGE_REQUEST_IID")
+
+    print()
+    getDiffFromMR(HOST, PROJECT_ID, STATE, PRIVATE_TOKEN, CONTENT_TYPE, IID)
 
 if __name__ == "__main__":
     main()
