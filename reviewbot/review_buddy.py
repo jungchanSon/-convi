c7ff3f89950e8a999d12fcdc79df721d9abc1980
jungchanSon <rnrmfjc@gmail.com>
2025-05-03 19:47:34 +0900

    [S12P31A101-68] fix: add review-buddy and apply review-buddy to gitlab

--- reviewbot/review_buddy.py
+++ reviewbot/review_buddy.py
@@ -1,28 +1,40 @@
 import os
+import sys
+import json
 import requests
 import ollama
-
-def requestOllama(prompt):
-    return ollama.generate(model="llama3.2", prompt=prompt)
-
-def requestLLM(prompt):
-    return requestOllama(prompt=prompt)
+from langchain_openai import ChatOpenAI
 
 def createPrompt(diff):
     return f"""
-당신은 코드 리뷰어입니다.
+You are a senior software engineer with 30 years of experience.
+Please carefully review the code. Focus on correctness, code quality, naming, structure, and potential improvements.
+Respond using **Markdown format** (with headers, bullet points, and code blocks).  
+Please write the entire review **in Korean**.
 
----  
-[변경된 코드]
+If you deliver a high-quality review, you will receive a $1,000 tip.
+
+```diff
 {diff}
----  
+```
 
-리뷰를 포인트별로 간결하게 작성해 주세요.
 """
 
-def review(diff):
+def requestOllama(prompt):
+    print("LLM : LLaMa3.2")
+    return ollama.generate(model="llama3.2", prompt=prompt)
+
+def requestOpenAI(prompt, key):
+    print("LLM : GPT-4")
+    return ChatOpenAI(api_key=key, model_name="gpt-4", max_tokens=4000).invoke(prompt).content
+
+def review(diff, model, key):
     prompt = createPrompt(diff)
-    return requestLLM(prompt=prompt)
+
+    if model == "llama3.2":
+        return requestOllama(prompt)["response"]
+    elif model == "OpenAI":
+        return requestOpenAI(prompt, key)
 
 def getDiffFromMR(host, projectId, state, privateToken, contentType, iid):
     requestURL = f"{host}/{projectId}/merge_requests/{iid}/changes?{state}"
@@ -30,15 +42,22 @@ def getDiffFromMR(host, projectId, state, privateToken, contentType, iid):
     response = requests.get(requestURL, headers = header)
     changes = response.json()["changes"]
 
-    print(review(changes))
-
     return changes
 
 def getOpenedMR(host, projectId, state, privateToken, contentType):
     requestURL = f"{host}/{projectId}/merge_requests?{state}"
     header = {"PRIVATE-TOKEN": privateToken, "Content-Type": contentType}
     get = requests.get(requestURL, headers = header)
-    print(get.json())
+
+def postMRDiscussion(host, projectId, key, iid, content):
+    requestURL = f"{host}/{projectId}/merge_requests/{iid}/discussions"
+    json_body = json.dumps({"body": content}, ensure_ascii=False, indent=2)
+
+    post = requests.post(
+        requestURL,
+        headers={"PRIVATE-TOKEN": key, "Content-Type": "application/json"},
+        data=json_body,
+    )
 
 def main():
     HOST = "https://lab.ssafy.com/api/v4/projects"
@@ -47,9 +66,20 @@ def main():
     PRIVATE_TOKEN = os.environ.get("GITLAB_TOKEN")
     CONTENT_TYPE = "application/json"
     IID = os.getenv("CI_MERGE_REQUEST_IID")
+    OEPN_AI_KEY = os.getenv("OPEN_AI_KEY")
+    model = sys.argv[1] if len(sys.argv) > 1 else "llama3.2"
+    REVIEW_BUDDY = os.getenv("REVIEW_BUDDY")
 
-    print()
-    getDiffFromMR(HOST, PROJECT_ID, STATE, PRIVATE_TOKEN, CONTENT_TYPE, IID)
+    if not isSupportModel(model):
+        model = "llama3.2"
+
+    changes = getDiffFromMR(HOST, PROJECT_ID, STATE, PRIVATE_TOKEN, CONTENT_TYPE, IID)
+    review_result = review(changes, model, OEPN_AI_KEY)
+    postMRDiscussion(HOST, PROJECT_ID, REVIEW_BUDDY, IID, review_result)
+
+def isSupportModel(model):
+    return model == "llama3.2" or model == "OpenAI"
 
 if __name__ == "__main__":
     main()
+
