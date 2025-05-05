98a21636638c238d0ab40258ece1d7740d274b85
jungchanSon <rnrmfjc@gmail.com>
2025-05-05 21:49:58 +0900

    [S12P31A101-78] feat: add ga tag

--- convi-frontend/app/layout.tsx
+++ convi-frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import GoogleAnalyticsTag from "@/features/GoogleAnalyticsTag";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,12 +23,16 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        {process.env.NEXT_PUBLIC_GA_ID && <GoogleAnalyticsTag/> }
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
