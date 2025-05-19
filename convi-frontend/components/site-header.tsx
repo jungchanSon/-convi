9c03f10f26d0c5dc7f8fb5c76138d867de4e6a64
jungchanSon <rnrmfjc@gmail.com>
2025-05-19 15:22:43 +0900

    [S12P31A101-96] feat: add SSAFY login button

--- convi-frontend/components/site-header.tsx
+++ convi-frontend/components/site-header.tsx
@@ -1,5 +1,6 @@
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
+import LoginComponent from "@/features/socialLogin/LoginComponent";
 
 type SiteHeaderPropsType = {
   siteTitle: string
@@ -7,15 +8,19 @@ type SiteHeaderPropsType = {
 
 export function SiteHeader({siteTitle} : SiteHeaderPropsType) {
   return (
-    <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
-      <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-        <SidebarTrigger className="-ml-1" />
-        <Separator
-          orientation="vertical"
-          className="mx-2 data-[orientation=vertical]:h-4"
-        />
-        <h1 className="text-base font-medium">{siteTitle}</h1>
-      </div>
+    <header className="flex justify-between h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+        <div className="flex items-center gap-1 px-4 lg:gap-2 lg:px-6">
+            <SidebarTrigger className="-ml-1"/>
+            <Separator
+                orientation="vertical"
+                className="mx-2 data-[orientation=vertical]:h-4"
+            />
+
+            <h1 className="text-base font-medium">{siteTitle}</h1>
+        </div>
+        <div>
+            <LoginComponent />
+        </div>
     </header>
   )
 }
